b224a9cef41a81e42b4f25ebc42c8ba4a159d0bd
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-25 11:21:26 -0400

    Regroup main.py scraper methods into one

--- main.py
+++ main.py
@@ -7,29 +7,15 @@ from scrapers.GithubScraper import *
 
 load_dotenv()
 
-def scrape_leaderboard(web_scraper, leaderboard_url, leaderboard_csv_file=None):
-	logging.info(f"Starting Code4rena leaderboard data scraping at '{leaderboard_url}'...")
-	df = web_scraper.scrape_leaderboard_table(leaderboard_url)
+def scrape(scrape_method, scrape_data_desc, url, csv_file=None):
+	logging.info(f"Starting {scrape_data_desc} data scraping at '{url}'...")
+	df = scrape_method(url)
 	
-	if (leaderboard_csv_file):
-		df.to_csv(leaderboard_csv_file, index=False)
+	if (csv_file):
+		df.to_csv(csv_file, index=False)
 	
-	logging.info(f"Finished Code4rena leaderboard data scraping: got '{len(df.index)}' rows of data [success]")
-	return df
-
-def scrape_contests(web_scraper, contests_url, contests_csv_file=None):
-	logging.info(f"Starting Code4rena contests data scraping at '{contests_url}'...")
-	df = web_scraper.scrape_contests_data(contests_url)
-	
-	if (contests_csv_file):
-		df.to_csv(contests_csv_file, index=False)
-	
-	logging.info(f"Finished Code4rena contests data scraping: wrote '{len(df.index)}' rows of data [success]")
-	return df
-
-def scrape_github(github_scraper):
-	logging.info(f"Starting Github data scraping for '{github_scraper.org}'...")
-	github_scraper.scrape_repos() # TODO : Make scraper return pandas DataFrame for consistency
+	logging.info(f"Finished {scrape_data_desc} data scraping: got {len(df.index)} rows of data [success]")
+	return df	
 
 if __name__ == "__main__":
 	file_handler = logging.FileHandler("code4rena.log", mode='w', encoding='utf8')
@@ -53,18 +39,22 @@ if __name__ == "__main__":
 
 	contests_url = "https://code4rena.com/contests"
 	contests_csv_file = 'contests_code4rena.csv'
+
+	github_org = "code-423n4"
+	github_csv_file = 'github_code4rena.csv'
 	
-	web_scraper = WebScraper(console_handler)
 	github_scraper = GithubScraper(console_handler)
 	target = sys.argv[1].lower() # TODO : Parse command line arguments
 
-	if (target == 'leaderboard'):
-		scrape_leaderboard(web_scraper, leaderboard_url, leaderboard_csv_file)
-	elif (target == 'contests'):
-		scrape_contests(web_scraper, contests_url, contests_csv_file)
-	elif (target == 'github'):
-		scrape_github(github_scraper)
+	if (target == 'github'):
+		scrape(github_scraper.scrape_repos, "Github repos", github_org, github_csv_file)
 	else:
-		scrape_leaderboard(web_scraper, leaderboard_url, leaderboard_csv_file)
-		scrape_contests(web_scraper, contests_url, contests_csv_file)
-		scrape_github(github_scraper)
+		web_scraper = WebScraper(console_handler) # Initialize Selenium driver only if needed
+		if (target == 'leaderboard'):
+			scrape(web_scraper.scrape_leaderboard_table, "Code4rena leaderboard", leaderboard_url, leaderboard_csv_file)
+		elif (target == 'contests'):
+			scrape(web_scraper.scrape_contests_data, "Code4rena contests", contests_url, contests_csv_file)
+		else:
+			scrape(web_scraper.scrape_leaderboard_table, "Code4rena leaderboard", leaderboard_url, leaderboard_csv_file)
+			scrape(web_scraper.scrape_contests_data, "Code4rena contests", contests_url, contests_csv_file)
+			scrape(github_scraper.scrape_repos, "Github repos", github_org, github_csv_file)
